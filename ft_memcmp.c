35bd65860b288ddbb66f3718a5a5fa15e4986c0a
Guillaume Roubaud <groubaud@mi-r1-p1.s19.be>
2021-04-06 18:56:37 +0200

    ft_memcmp.c vs guacamole, done

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: groubaud <groubaud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 17:24:17 by groubaud          #+#    #+#             */
-/*   Updated: 2021/04/01 17:24:17 by groubaud         ###   ########.fr       */
+/*   Updated: 2021/04/06 18:55:48 by groubaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	if (n == 0)
 		return (0);
-	n--;
 	index = 0;
 	tmp1 = (unsigned char *)s1;
 	tmp2 = (unsigned char *)s2;
@@ -28,9 +27,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		index++;
 	if (index == n)
 		return (0);
-	if (tmp1[index] > tmp2[index])
-		return (1);
-	else if (tmp1[index] < tmp2[index])
-		return (-1);
-	return (0);
+	return (tmp1[index] - tmp2[index]);
 }
