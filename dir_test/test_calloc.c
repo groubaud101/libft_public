fafbbac8e2efd10aaf57757cc4318f1d7275d464
Guillaume Roubaud <guroubau@shi-r5-p1.s19.be>
2022-10-11 19:30:50 +0200

    DONE calloc overflow

--- dir_test/test_calloc.c
+++ dir_test/test_calloc.c
@@ -2,8 +2,11 @@
 
 int		test_calloc(void)
 {
-	char	*str1;
-	char	*str2;
+	char	*str1 = NULL;
+	char	*str2 = NULL;
+	size_t	nb1;
+	size_t	nb2;
+
 
 	str1 = (char *)calloc(sizeof(char), 10);
 	str2 = (char *)ft_calloc(sizeof(char), 10);
@@ -12,5 +15,52 @@ int		test_calloc(void)
 	printf("   calloc : |%s|\nft_calloc : |%s|\n", str1, str2);
 	test_free(str1, str2, 0, 0);
 
+	nb1 = 1234567890123456789;
+	nb2 = 123;
+	printf("nb1 * nb2 = %zu\n", nb1 * nb2);
+	printf("ori : |%s|\n", (str1 = calloc(nb1, nb2)));
+	free(str1);
+	printf("ft_ : |%s|\n", (str2 = ft_calloc(nb1, nb2)));
+	free(str2);
+
+	nb1 = 200000000;
+	nb2 = 3000000;
+	printf("nb1 * nb2 = %zu\n", nb1 * nb2);
+	printf("ori : |%s|\n", (str1 = calloc(nb1, nb2)));
+	free(str1);
+	printf("ft_ : |%s|\n", (str2 = ft_calloc(nb1, nb2)));
+	free(str2);
+
+	nb1 = 1234567890123456789;
+	nb2 = 10;
+	printf("\nnb1 * nb2 = %zu\n", nb1 * nb2);
+	printf("ori : |%s|\n", (str1 = calloc(nb1, nb2)));
+	free(str1);
+	printf("ft_ : |%s|\n", (str2 = ft_calloc(nb1, nb2)));
+	free(str2);
+
+	nb1 = SIZE_MAX;
+	nb2 = 1;
+	printf("\nnb1 * nb2 = %zu\n", nb1 * nb2);
+	printf("ori : |%s|\n", (str1 = calloc(nb1, nb2)));
+	free(str1);
+	printf("ft_ : |%s|\n", (str2 = ft_calloc(nb1, nb2)));
+	free(str2);
+
+	nb1 = SIZE_MAX / 10;
+	nb2 = 10;
+	printf("\nnb1 * nb2 = %zu\n", nb1 * nb2);
+	printf("ori : |%s|\n", (str1 = calloc(nb1, nb2)));
+	free(str1);
+	printf("ft_ : |%s|\n", (str2 = ft_calloc(nb1, nb2)));
+	free(str2);
+
+	nb1 = SIZE_MAX / 2;
+	nb2 = 2;
+	printf("\nnb1 * nb2 = %zu\n", nb1 * nb2);
+	printf("ori : |%s|\n", (str1 = calloc(nb1, nb2)));
+	free(str1);
+	printf("ft_ : |%s|\n", (str2 = ft_calloc(nb1, nb2)));
+	free(str2);
 	return (1);
 }
--- ft_calloc.c
+++ ft_calloc.c
@@ -3,14 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: groubaud <groubaud@student.s19.be>         +#+  +:+       +#+        */
+/*   By: guroubau <guroubau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/01 17:23:33 by groubaud          #+#    #+#             */
-/*   Updated: 2021/04/01 17:23:33 by groubaud         ###   ########.fr       */
+/*   Created: 2022/10/03 12:05:16 by guroubau          #+#    #+#             */
+/*   Updated: 2022/10/03 12:05:16 by guroubau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdint.h>
+
+static int	ft_nbrlen_size_t(size_t n)
+{
+	int	len;
+
+	len = 0;
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+static int	ft_check_mult_len_size(size_t nb1, size_t nb2)
+{
+	int		len_nb1;
+	int		len_nb2;
+	int		len_size_max;
+	size_t	size_max;
+
+	size_max = -1;
+	len_nb1 = ft_nbrlen_size_t(nb1);
+	len_nb2 = ft_nbrlen_size_t(nb2);
+	len_size_max = ft_nbrlen_size_t(size_max);
+	if (len_nb1 + len_nb2 > len_size_max + 1)
+		return (1);
+	else if (len_nb1 + len_nb2 < len_size_max)
+		return (-1);
+	return (0);
+}
+
+static int	ft_check_is_mult_overflowing(size_t nb1, size_t nb2)
+{
+	int		check;
+	size_t	bigger;
+	size_t	smaller;
+
+	if (nb1 == 0 || nb2 == 0)
+		return (0);
+	check = ft_check_mult_len_size(nb1, nb2);
+	if (check == 1)
+		return (1);
+	else if (check == -1)
+		return (0);
+	bigger = nb1;
+	smaller = nb2;
+	if (nb1 < nb2)
+	{
+		bigger = nb2;
+		smaller = nb1;
+	}
+	if ((((bigger / 10) * smaller) > SIZE_MAX / 10)
+		|| ((((bigger / 10) * smaller) == SIZE_MAX / 10)
+			&& (((bigger % 10) * (smaller % 10)) % 10 > SIZE_MAX % 10)))
+		return (1);
+	return (0);
+}
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
@@ -18,6 +77,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	index;
 	size_t	mult;
 
+	if (ft_check_is_mult_overflowing(nmemb, size) == 1)
+		return (NULL);
 	index = 0;
 	mult = nmemb * size;
 	array = (char *)malloc(mult);
